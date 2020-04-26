4e5c2ec781a5f7ea4782b32148fb90eeea1824b4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-26 23:28:57 +0000

    Deploy ospeople/note to github.com/ospeople/note.git:gh-pages

---
+++ precache-manifest.df5ea4b9b02b85583ba36fe956734a18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aaa6a6eb79e3998cf47e53c29a843b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c75ce0b4d9c4e51eef2d",
+    "url": "./static/js/2.ef476786.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ef476786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56610eacf94412cba3b6",
+    "url": "./static/js/main.9ea40d73.chunk.js"
+  },
+  {
+    "revision": "2bb2e5922fb1b489f158",
+    "url": "./static/js/runtime-main.f0afd649.js"
+  }
+]);
